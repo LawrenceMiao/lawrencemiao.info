eea653e10879b4cbfdbd71bde455a59ad62440cb
Lawrence M <lawrencejunzh@gmail.com>
2024-06-19 13:30:18 -0400

    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save o redload.
+          This is lawrence react website
         </p>
         <a
           className="App-link"
