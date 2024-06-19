593ceaf122c060e06b3a3459ed81056908fae806
Lawrence M <lawrencejunzh@gmail.com>
2024-06-19 11:02:48 -0400

    a

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to redload.
+          Edit <code>src/App.js</code> and save o redload.
         </p>
         <a
           className="App-link"
