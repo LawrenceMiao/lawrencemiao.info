0611a19ef51310bb2adbe07ae447b304bff31cdc
Lawrence M <lawrencejunzh@gmail.com>
2024-06-29 23:46:05 -0400

    update favicon

--- src/pages/NoPage.js
+++ src/pages/NoPage.js
@@ -1,6 +1,12 @@
 const NoPage = () => {
-    return <h1 className="bg-slate-600">404</h1>;
-  };
-  
-  export default NoPage;
-  
+    return (
+        <>
+            <h1 className="bg-slate-600">Page not found.</h1>
+            <p>
+                Hope no one see's this because I'm too lazy to format this page.
+            </p>
+        </>
+    )
+}
+
+export default NoPage
