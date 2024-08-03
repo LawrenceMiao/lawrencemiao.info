f5e2f52f1700d2d155708a8e0997b176f13525e3
Lawrence M <lawrencejunzh@gmail.com>
2024-08-03 12:53:31 -0400

    fix the navbar

--- src/App.js
+++ src/App.js
@@ -32,123 +32,128 @@ function App() {
 
   return (
     <>
-      <NavBar parallaxRef={parallaxRef} />
-      <div style={{ height: '100vh' }}>
-        <Parallax pages={4}
-          config={massless}
-          style={{ backgroundColor: "white" }}
-          ref={parallaxRef}
+
+      <div style={{ position: "sticky", zIndex: 1000 }}>
+        <NavBar parallaxRef={parallaxRef}
+        />
+      </div>
+      <Parallax className="top-0" pages={4}
+        config={massless}
+        style={{ backgroundColor: "white" }}
+        ref={parallaxRef}
+      >
+
+        {/* First Section - Black Background */}
+        <ParallaxLayer
+          offset={0}
+          speed={0.25}
+          style={{ backgroundColor: 'black', height: "75vw" }}
+
         >
-          {/* First Section - Black Background */}
-          <ParallaxLayer
-            offset={0}
-            speed={0.25}
-            style={{ backgroundColor: 'black', height: "75vw" }}
+        </ParallaxLayer>
+        {/* Lawrence Miao */}
+        <ParallaxLayer
+          offset={0}
+          speed={0}
+          className='text-white bg-transparent text-center font-thin text-9xl pt-[14vw]'
 
-          >
-          </ParallaxLayer>
-          {/* Lawrence Miao */}
-          <ParallaxLayer
-            offset={0}
-            speed={0}
-            className='text-white bg-transparent text-center font-thin text-9xl pt-[14vw]'
+        >
+          <h1>
+            Lawrence Miao
+          </h1>
+        </ParallaxLayer>
 
-          >
-            <h1>
-              Lawrence Miao
-            </h1>
-          </ParallaxLayer>
+        {/* Red Sun */}
+        <ParallaxLayer
+          offset={0.5}
+          speed={1.5}
+          config={massless}
+        >
+          <svg
+            width="100vw"
+            height="75vw"
+            viewBox={`0 0 ${width} ${width}`}
+            style={{ display: 'block', backgroundColor: "transparent" }}>
 
-          {/* Red Sun */}
-          <ParallaxLayer
-            offset={0.5}
-            speed={1.5}
-            config={massless}
-          >
-            <svg
-              width="100vw"
-              height="75vw"
-              viewBox={`0 0 ${width} ${width}`}
-              style={{ display: 'block', backgroundColor: "transparent" }}>
+            <circle
+              cx="30vw"   // Center X coordinate
+              cy="40vw"   // Center Y coordinate
+              r="15vw"    // Radius
+              fill="#FF3C00" // Fill color
+            />
+          </svg>
+        </ParallaxLayer>
+        {/* Mountain */}
+        <ParallaxLayer
+          offset={0}
+          speed={0.25}
+          config={massless}
+        >
+          <svg
+            width="100vw"
+            height="75vw"
+            viewBox={`0 0 ${width} ${width * 0.75}`}
+            style={{ display: 'block', backgroundColor: "transparent" }}>
 
-              <circle
-                cx="30vw"   // Center X coordinate
-                cy="40vw"   // Center Y coordinate
-                r="15vw"    // Radius
-                fill="#FF3C00" // Fill color
-              />
-            </svg>
-          </ParallaxLayer>
-          {/* Mountain */}
-          <ParallaxLayer
-            offset={0}
-            speed={0.25}
-            config={massless}
-          >
-            <svg
-              width="100vw"
-              height="75vw"
-              viewBox={`0 0 ${width} ${width * 0.75}`}
-              style={{ display: 'block', backgroundColor: "transparent" }}>
+            <polygon
+              points={`0,${width * 0.75} ${width / 2},${width * 0.25} ${width},${width * 0.75}`}
+              fill="white"
+            />
+          </svg>
+        </ParallaxLayer>
 
-              <polygon
-                points={`0,${width * 0.75} ${width / 2},${width * 0.25} ${width},${width * 0.75}`}
-                fill="white"
-              />
-            </svg>
-          </ParallaxLayer>
+        <ParallaxLayer
+          offset={1}
+          speed={1}
+          style={{ backgroundColor: 'transparent' }}
+          config={massless}
 
-          <ParallaxLayer
-            offset={1}
-            speed={1}
-            style={{ backgroundColor: 'transparent' }}
-            config={massless}
+        >
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            height: '100%',
+            color: 'black'
+          }}>
+            <h1>About</h1>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={2}
+          speed={1}
+          style={{ backgroundColor: 'transparent' }}
+          config={massless}
 
-          >
-            <div style={{
-              display: 'flex',
-              justifyContent: 'center',
-              height: '100%',
-              color: 'black'
-            }}>
-              <h1>About</h1>
-            </div>
-          </ParallaxLayer>
-          <ParallaxLayer
-            offset={2}
-            speed={1}
-            style={{ backgroundColor: 'transparent' }}
-            config={massless}
+        >
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            height: '100%',
+            color: 'black'
+          }} >
+            <h1>Projects</h1>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={3}
+          speed={1}
+          style={{ backgroundColor: 'transparent' }}
+          config={massless}
 
-          >
-            <div style={{
-              display: 'flex',
-              justifyContent: 'center',
-              height: '100%',
-              color: 'black'
-            }} >
-              <h1>Projects</h1>
-            </div>
-          </ParallaxLayer>
-          <ParallaxLayer
-            offset={3}
-            speed={1}
-            style={{ backgroundColor: 'transparent' }}
-            config={massless}
+        >
+          <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            height: '100%',
+            color: 'black'
+          }}>
+            <h1>Work Experience</h1>
+          </div>
+        </ParallaxLayer>
 
-          >
-            <div style={{
-              display: 'flex',
-              justifyContent: 'center',
-              height: '100%',
-              color: 'black'
-            }}>
-              <h1>Work Experience</h1>
-            </div>
-          </ParallaxLayer>
+      </Parallax>
 
-        </Parallax>
-      </div >
+
     </>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,6 +2,32 @@ import * as React from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 import Box from '@mui/material/Box';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+
+const customTheme = createTheme({
+    palette: {
+        primary: {
+            main: '#dc004e', // Custom primary color
+        },
+        // secondary: {
+        //     main: '#dc004e', // Custom secondary color
+        // },
+        background: {
+            default: '#f5f5f5', // Custom background color
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: '8px', // Custom border radius for buttons
+                },
+            },
+        },
+    },
+});
 
 
 function a11yProps(index) {
@@ -23,23 +49,29 @@ export default function NavBar(props) {
     };
 
     return (
-        <Box sx={{ width: '100%' }}>
-            <Box sx={{ borderBottom: 1, borderColor: 'divider', backgroundColor: "#111111" }}>
-                <Tabs value={value} onChange={handleChange}
-                    aria-label="basic tabs example" textColor='secondary'
-                    sx={{
-                        ".Mui-selected": {
-                            color: '#FF3C00',
-                        },
-                    }}>
+        <ThemeProvider theme={customTheme}>
+            <CssBaseline />
+            {/* <Box sx={{ width: '100%' }}> */}
+            <Box sx={{
+                width: '100%', borderBottom: 2,
+                borderColor: 'divider',
+                background: 'rgba(255, 255, 255, 0.5)', // White background with transparency
+                backdropFilter: 'blur(8px)',
+            }}>
+                <Tabs sx={{ width: '100%' }} value={value} onChange={handleChange}
+                    aria-label="basic tabs example"
+                >
                     <Tab onClick={() => scrollToOffset(1)}
                         label="About" {...a11yProps(0)} />
                     <Tab onClick={() => scrollToOffset(3)}
                         label="Work Experience" {...a11yProps(1)} />
                     <Tab onClick={() => scrollToOffset(2)}
                         label="Projects" {...a11yProps(2)} />
+                    <Tab onClick={() => scrollToOffset(2)}
+                        label="Contact" {...a11yProps(2)} />
                 </Tabs>
             </Box>
-        </Box>
+            {/* </Box> */}
+        </ThemeProvider>
     );
 }
